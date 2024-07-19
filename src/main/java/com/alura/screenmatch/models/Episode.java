68e618eee9c6248b0de347f572d19e1f271ea2a9
Heithor Urquiza <111160912+HeithorUrquiza@users.noreply.github.com>
2024-07-19 15:07:19 -0300

    feat: Implementing Episode model and update Pricipal methods

---
+++ src/main/java/com/alura/screenmatch/models/Episode.java
@@ -0,0 +1,78 @@
+package com.alura.screenmatch.models;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episode {
+    private Integer season;
+    private String title;
+    private Integer episodeNumber;
+    private Double rating;
+    private LocalDate released;
+
+    public Episode(Integer season, EpisodeData episodeData) {
+        this.season = season;
+        this.title = episodeData.title();
+        this.episodeNumber = episodeData.episode();
+        try {
+            this.rating = Double.valueOf(episodeData.rating());
+        }
+        catch(NumberFormatException exception) {
+            this.rating = 0.0;
+        }
+        try {
+            // DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            this.released = LocalDate.parse(episodeData.released());
+        }
+        catch(DateTimeParseException exception) {
+            this.released = null;
+        }
+    }
+
+    public Integer getSeason() {
+        return season;
+    }
+
+    public void setSeason(Integer season) {
+        this.season = season;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getEpisodeNumber() {
+        return episodeNumber;
+    }
+
+    public void setEpisodeNumber(Integer episodeNumber) {
+        this.episodeNumber = episodeNumber;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public LocalDate getReleased() {
+        return released;
+    }
+
+    public void setReleased(LocalDate released) {
+        this.released = released;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("season=%d, title=%s, episodeNumber=%d, rating=%.1f, released=%s",
+                this.season, this.title, this.episodeNumber, this.rating, this.released);
+    }
+}
+
--- src/main/java/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/com/alura/screenmatch/principal/Principal.java
@@ -1,48 +1,92 @@
 package com.alura.screenmatch.principal;
 
+import com.alura.screenmatch.models.Episode;
+import com.alura.screenmatch.models.EpisodeData;
 import com.alura.screenmatch.models.SeasonData;
 import com.alura.screenmatch.models.SerieData;
 import com.alura.screenmatch.services.ApiConsumer;
 import com.alura.screenmatch.services.DataConversor;
 import io.github.cdimascio.dotenv.Dotenv;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner scanner = new Scanner(System.in);
     private ApiConsumer apiConsumer = new ApiConsumer();
     private DataConversor dataConversor = new DataConversor();
     private List<SeasonData> seasons = new ArrayList<>();
+    private List<Episode> episodes = new ArrayList<>();
 
     // Constants
     private final Dotenv dotenv = Dotenv.load();
     private final String API_KEY = dotenv.get("IMDB_API_KEY");
-    private final String BASE_URL = "https://www.omdbapi.com/?t=";
 
-    public void showMenu() {
+    public void starter() {
+        this.collectData();
+        this.episodes = this.populate();
+//        this.showAllEpisodesTitles(episodes);
+//        this.getTop5Episodes(episodes);
+        this.getEpisodesFromYear(episodes);
+    }
+
+    public void collectData() {
+        final String BASE_URL = "https://www.omdbapi.com/?t=";
         System.out.print("Insert the series name: ");
-        String serieName = scanner.nextLine();
+        String serieName = this.scanner.nextLine();
+
         // Getting serie data
-        String serieJson = apiConsumer.getData(
+        String serieJson = this.apiConsumer.getData(
                 BASE_URL +
                         serieName.replace(" ", "+") +
                         "&apiKey=" + API_KEY
         );
-        SerieData serieData = dataConversor.getData(serieJson, SerieData.class);
-        System.out.println(serieData);
+        SerieData serieData = this.dataConversor.getData(serieJson, SerieData.class);
 
         // Getting season data
         for (int i = 1; i <= serieData.totalSeasons(); i++) {
-            String seasonJson = apiConsumer.getData(
+            String seasonJson = this.apiConsumer.getData(
                     BASE_URL +
                             serieName.replace(" ", "+") +
                             "&season=" + i +
                             "&apiKey=" + API_KEY
             );
-            seasons.add(dataConversor.getData(seasonJson, SeasonData.class));
+            this.seasons.add(dataConversor.getData(seasonJson, SeasonData.class));
         }
-        seasons.forEach(System.out::println);
+    }
+
+    public List<Episode> populate() {
+        return this.seasons.stream()
+                .flatMap(season -> season.episodes().stream()
+                        .map(episode -> new Episode(season.season(), episode)))
+                .collect(Collectors.toList());
+    }
+
+    public void showAllEpisodesTitles(List<Episode> episodes) {
+        episodes.stream()
+                .sorted(Comparator.comparing(Episode::getSeason))
+                .forEach(episode -> System.out.printf("S%d - %s\n", episode.getSeason(), episode.getTitle()));
+    }
+
+    public void getTop5Episodes(List<Episode> episodes) {
+        System.out.println("Top 5 episodes !!");
+        episodes.stream()
+                .sorted(Comparator.comparing(Episode::getRating).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+    }
+
+    public void getEpisodesFromYear(List<Episode> episodes) {
+        System.out.print("Chose a year for search: ");
+        int year = this.scanner.nextInt();
+        LocalDate searchDate = LocalDate.of(year, 1, 1);
+        episodes.stream()
+                .filter(episode -> episode.getReleased() != null && episode.getReleased().isAfter(searchDate))
+                .forEach(episode -> System.out.printf("Season: %d | Episode: %d | Released: %s\n",
+                        episode.getSeason(), episode.getEpisodeNumber(), episode.getReleased()));
     }
 }
